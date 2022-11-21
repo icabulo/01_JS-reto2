df9dca7f6af16bc3d2ca3fcd4c7f82806e1c83c6
Ivan <icabulo@gmail.com>
2022-11-21 12:22:41 -0500

    checkPropecty function modified with ternary operator

--- checkProperty.js
+++ checkProperty.js
@@ -1,9 +1,6 @@
-console.log(myFunction({ a: 1, b: 2, c: 3 }, 'b'));
-console.log(myFunction({ x: 'a', y: 'b', z: 'c' }, 'a'));
-console.log(myFunction({ x: 'a', y: 'b', z: undefined }, 'z'));
+console.log(myFunction({ a: 1, b: 2, c: 3 }, "b"));
+console.log(myFunction({ x: "a", y: "b", z: "c" }, "a"));
+console.log(myFunction({ x: "a", y: "b", z: undefined }, "z"));
 function myFunction(a, b) {
-  if (a[b] == undefined) {
-    return false;
-  }
-  else return true
+  a[b] == undefined ? false : true;
 }
